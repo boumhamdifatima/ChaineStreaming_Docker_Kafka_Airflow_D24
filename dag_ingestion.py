3b186d13896347c817e7a954f3def0de1477dda5
unknown <boumhamdifati@gmail.com>
2025-03-16 00:53:06 -0400

    Ajout des descriptions des fonctions python dans dag_ingestion.py

--- dag_ingestion.py
+++ dag_ingestion.py
@@ -26,7 +26,29 @@ dag = DAG(
 
 # Tache 1: Production et envoi des messages vers kafka
 def producer_data():
+    '''
+    Génère et envoie des messages simulés vers un topic Kafka.
 
+    Cette fonction crée des données fictives sur les paramètres vitaux des patients
+    (ID, nom, âge, pression artérielle, fréquence cardiaque, température, timestamp)
+    et les envoie à un topic Kafka en continu.
+
+    Fonctionnement :
+    - Utilise la bibliothèque Faker pour générer des données médicales fictives.
+    - Configure un producteur Kafka pour envoyer les messages.
+    - Envoie 20 messages à intervalles de 0,5 seconde vers le topic Kafka 'real_time_medical_messages'.
+    
+    Données générées par message :
+    - patient_id (UUID) : Identifiant unique du patient.
+    - name (str) : Nom complet du patient.
+    - age (int) : Âge du patient (entre 20 et 90 ans).
+    - blood_pressure (str) : Pression artérielle sous format "systolique/diastolique".
+    - heart_rate (int) : Fréquence cardiaque (entre 50 et 120 bpm).
+    - temperature (float) : Température corporelle (entre 36.0 et 40.0 °C).
+    - timestamp (float) : Horodatage Unix du moment d’envoi du message.
+
+    Cette fonction est utile pour tester un pipeline de traitement en temps réel basé sur Kafka.
+    '''
     # Initialiser Faker
     fake = Faker()
 
@@ -61,6 +83,36 @@ def producer_data():
 
 # Tache 2: Consommation des messages a partir de kafka
 def consumer_data(**kwargs):
+    '''
+    Consomme des messages depuis un topic Kafka et les stocke via XCom pour les étapes suivantes.
+
+    Cette fonction écoute en temps réel les messages envoyés au topic Kafka 
+    'real_time_medical_messages' et récupère jusqu'à 15 messages avant de s'arrêter.
+
+    Fonctionnement :
+    - Configure un consommateur Kafka connecté au broker 'kafka:9092'.
+    - Écoute les messages provenant du topic spécifié.
+    - Ajoute chaque message reçu à une liste et affiche son contenu.
+    - Arrête la consommation après avoir reçu 15 messages.
+    - Utilise XCom pour stocker les messages et les transmettre aux prochaines tâches Airflow.
+    - Ferme proprement le consommateur après l'arrêt.
+
+    Paramètres :
+    - kwargs (dict) : Contient les informations du contexte d'exécution d'Airflow.
+                      La clé 'ti' est utilisée pour stocker les messages avec XCom.
+
+    Données traitées :
+    - patient_id (UUID) : Identifiant unique du patient.
+    - name (str) : Nom complet du patient.
+    - age (int) : Âge du patient.
+    - blood_pressure (str) : Pression artérielle du patient.
+    - heart_rate (int) : Fréquence cardiaque du patient.
+    - temperature (float) : Température corporelle en °C.
+    - timestamp (float) : Horodatage du message.
+
+    Cette fonction est conçue pour être utilisée dans un pipeline Airflow avec Kafka 
+    pour le traitement en temps réel des paramètres vitaux des patients.
+    '''
     
     # Récupérer la tâche instance pour XCom
     ti = kwargs['ti']
@@ -94,6 +146,17 @@ def consumer_data(**kwargs):
     
 # Tache 3: Extraction du prénom et nom a partir de "name" 
 def transform_extract_name(**kwargs):
+    '''
+    Cette fonction extrait et transforme les noms complets des messages reçus, en les divisant en prénoms et noms de famille.
+    Elle réorganise ensuite les données des messages, en mettant les prénoms et noms de famille en premier dans un format structuré.
+    Les messages transformés sont ensuite envoyés à un autre processus via XCom.
+
+    Paramètres :
+        **kwargs: Arguments passés à la fonction, y compris l'objet 'ti' permettant d'accéder aux données du contexte du task.
+        
+    Retourne :
+        Rien. La fonction pousse les données transformées vers XCom pour être utilisées par d'autres tâches.
+    '''
     ti = kwargs['ti']
     messages = ti.xcom_pull(key='messages', task_ids='consommer_data')
     
@@ -127,6 +190,16 @@ def transform_extract_name(**kwargs):
 
 # Tache 4: Formatage du timestamp
 def transform_format_timestamp(**kwargs):
+    '''
+    Cette fonction prend une liste de messages contenant des timestamps sous forme de valeurs UNIX et les transforme en un format lisible 
+    (YYYY-MM-DD HH:MM:SS). Elle modifie les messages en conséquence et les renvoie dans un format transformé.
+
+    Paramètres :
+        **kwargs: Arguments passés à la fonction, y compris l'objet 'ti' permettant d'accéder aux données du contexte du task.
+        
+    Retourne :
+        Rien. La fonction pousse les messages transformés (avec des timestamps formatés) vers XCom pour être utilisés par d'autres tâches.
+    '''
     ti = kwargs['ti']
     messages = ti.xcom_pull(key='messages_transformed', task_ids='transformer_name')
     
@@ -142,7 +215,21 @@ def transform_format_timestamp(**kwargs):
 
 # Fonction qui retourne l'état de pression en fonction de l'age et la pression artérielle 
 def classify_blood_pressure(blood_pressure, age):
-    """Détermine l'état de la pression artérielle en fonction de l'âge et de la pression."""
+    '''
+    Détermine l'état de la pression artérielle en fonction de l'âge et de la pression artérielle systolique et diastolique.
+    La classification est effectuée selon les seuils suivants :
+    - Faible (Low) : systolique < 90 ou diastolique < 60
+    - Normale (Normal) : entre les seuils bas et élevés selon l'âge
+    - Haute (High) : systolique >= 130 ou diastolique >= 80 pour les jeunes adultes, 
+      systolique >= 140 ou diastolique >= 90 pour les adultes d'âge moyen, et systolique >= 150 ou diastolique >= 90 pour les personnes âgées.
+    
+    Paramètres :
+        blood_pressure (str) : La pression artérielle sous forme de chaîne de caractères, avec la systolique et la diastolique séparées par "/".
+        age (int) : L'âge du patient.
+    
+    Retourne :
+        str : Le statut de la pression artérielle ("Low", "Normal", "High").
+    '''
     systolic, diastolic = map(int, blood_pressure.split("/"))
 
     if age < 40:  # Jeunes adultes (18-39 ans)
@@ -169,6 +256,17 @@ def classify_blood_pressure(blood_pressure, age):
 
 # Tache 5: Calcul et ajout de l'etat de la pression artérielle 
 def transform_add_blood_pressure_status(**kwargs):
+    '''
+    Cette fonction ajoute un statut de pression artérielle à chaque message en fonction de la pression artérielle et de l'âge du patient.
+    Le statut est déterminé par la fonction `classify_blood_pressure` et est ajouté sous la clé "blood_pressure_status".
+    Ensuite, la fonction réorganise les données des messages pour inclure ce statut après la pression artérielle.
+
+    Paramètres :
+        **kwargs : Arguments passés à la fonction, y compris l'objet 'ti' permettant d'accéder aux données du contexte du task.
+        
+    Retourne :
+        Rien. La fonction pousse les messages transformés (avec le statut de pression artérielle ajouté) vers XCom pour être utilisés par d'autres tâches.
+    '''
     ti = kwargs['ti']
     messages = ti.xcom_pull(key='messages_transformed', task_ids='transformer_format_timestamp')
 
@@ -221,7 +319,17 @@ def transform_add_blood_pressure_status(**kwargs):
 
 # Tache 6: Stockage des messages dans Redis sous forme de liste.
 def store_messages_in_redis(**kwargs):
-    """Stocke les messages transformés dans Redis sous forme de liste."""
+    '''
+    Cette fonction stocke les messages transformés dans Redis sous forme de liste. 
+    Chaque message est sérialisé en JSON et ajouté à une liste Redis unique. 
+    Redis est utilisé ici pour sauvegarder les données de manière persistante, afin qu'elles puissent être récupérées par d'autres processus.
+
+    Paramètres :
+        **kwargs : Arguments passés à la fonction, y compris l'objet 'ti' permettant d'accéder aux données du contexte du task.
+        
+    Retourne :
+        Rien. La fonction pousse les messages dans Redis pour un stockage durable.
+    '''
     ti = kwargs['ti']
     messages = ti.xcom_pull(key='messages_final', task_ids='ajouter_etat_pression')
 
@@ -256,7 +364,6 @@ tache_1 = PythonOperator(
 tache_2 = PythonOperator(
     task_id='consommer_data',
     python_callable=consumer_data,
-    #provide_context=True,  # Passe le contexte Airflow
     dag=dag
 )
 
@@ -264,7 +371,6 @@ tache_2 = PythonOperator(
 tache_3 = PythonOperator(
     task_id='transformer_name',
     python_callable=transform_extract_name,
-    #provide_context=True,  # Passe le contexte Airflow
     dag=dag
 )
 
@@ -272,7 +378,6 @@ tache_3 = PythonOperator(
 tache_4 = PythonOperator(
     task_id='transformer_format_timestamp',
     python_callable=transform_format_timestamp,
-    #provide_context=True,  # Passe le contexte Airflow
     dag=dag
 )
 
@@ -280,7 +385,6 @@ tache_4 = PythonOperator(
 tache_5 = PythonOperator(
     task_id='ajouter_etat_pression',
     python_callable=transform_add_blood_pressure_status,
-    #provide_context=True,  # Passe le contexte Airflow
     dag=dag
 )
 
@@ -288,7 +392,6 @@ tache_5 = PythonOperator(
 tache_6 = PythonOperator(
     task_id="stocker_data_redis",
     python_callable=store_messages_in_redis,
-    #provide_context=True,
     dag=dag
 )
 
